90b2ad6f38bd207305c3ed7711fe1c1aae29bf43
hector.riv <books_thief@outlook.com>
2023-09-01 11:23:47 -0600

    Update: Change Intel EPB from 6 to 4

--- chintel-performance.sh
+++ chintel-performance.sh
@@ -3,5 +3,5 @@
 
 # notify-send is optional to send notifications
 
-echo 6 | tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
+echo 4 | tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
 echo default | tee /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference
